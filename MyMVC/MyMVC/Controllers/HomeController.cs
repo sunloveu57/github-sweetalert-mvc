b6f1c2e2f116d0a5be33c027b9c8bd758a91db81
sunloveu57 <sunloveu57@gmail.com>
2025-01-08 17:26:52 +0700

    First Commit

---
+++ MyMVC/MyMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMVC.Models;
+using System.Diagnostics;
+
+namespace MyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Name)
+        {
+            return RedirectToAction("Completed", new { Name });
+        }
+
+        public IActionResult Completed(string Name)
+        {
+            return View("Completed", Name);
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
